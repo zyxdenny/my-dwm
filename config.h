2839c848c999175d5f5f40270c19dba32c7ed33b
yuxuan <zyxdenny@gmail.com>
2023-10-22 19:14:49 +0800

    change MODE icon to [T] [F] [M]

--- config.h
+++ config.h
@@ -47,8 +47,8 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
+	{ "[T]",      tile },    /* first entry is default */
+	{ "[F]",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
 };
 
